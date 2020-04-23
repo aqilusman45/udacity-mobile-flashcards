18f319bfcb1552f3aadcd2d896ed338c91839aa2
aqilusman45 <aqilusman45@gmail.com>
2020-04-23 09:55:19 +0500

    feat: deck lis

---
+++ components/Decks/styled_components.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const NavBtn = styled.Button`
+  margin: 20px;
+`;
+
+export const ScrollableContainer = styled.FlatList``;
+
+export const ListItemContentWrapper = styled.View`
+  justify-content: center;
+  align-items: center;
+  border: none;
+  justify-content: center;
+  width: 90%;
+  margin: 10px auto;
+  padding: 15px;
+  border-radius: 22px;
+  background-color: #add8e6;
+  shadow-color: #000000;
+  shadow-opacity: 0.8;
+  shadow-radius: 2px;
+  box-shadow: 3px 3px;
+`;
+
+export const NavigateWrapper = styled.TouchableOpacity``;
