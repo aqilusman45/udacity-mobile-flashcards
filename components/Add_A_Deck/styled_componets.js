3d5bfa315eb7064759f6fb8fcd64c3b9b9c9ffc9
aqilusman45 <aqilusman45@gmail.com>
2020-04-23 09:54:11 +0500

    feat: add a deck styled commponents

---
+++ components/Add_A_Deck/styled_componets.js
@@ -0,0 +1,33 @@
+import styled from 'styled-components/native';
+
+export const DeckTitleInput = styled.TextInput`
+  width: 100%;
+  height: 50px;
+  background-color: #add8e6;
+  border: none;
+  border-radius: 22px;
+  margin-top: 25px;
+  font-size: 23px;
+  padding: 10px;
+  padding-left: 20px;
+  padding-right: 20px;
+`;
+
+export const AddDeckWrapper = styled.View`
+  flex: 1;
+  padding-top: 50px;
+  align-items: center;
+  margin-left: 10px;
+  margin-right: 10px;
+`;
+
+export const BtnContainer = styled.View`
+  margin-top: 30px;
+`;
+export const SubmitBtn = styled.Button`
+  width: 100%;
+  background-color: #2196f3;
+  padding: 10px;
+  padding-right: 40px;
+  padding-left: 40px;
+`;
