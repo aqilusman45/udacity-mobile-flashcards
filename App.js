7fbbca42640b8c7866c8a5896af03a721be3e5c3
aqilusman45 <aqilusman45@gmail.com>
2020-04-23 09:50:34 +0500

    feat: tab navigator

--- App.js
+++ App.js
@@ -1,19 +1,54 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Decks from "./components/Decks";
+import { Provider } from "react-redux";
+import store from "./store";
+import AddDeck from "./components/Add_A_Deck";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import AntDesign from "react-native-vector-icons/AntDesign";
+import { setLocalNotifications } from "./utils/helpers";
+const Tabs = createBottomTabNavigator();
 
 export default function App() {
+  useEffect(() => {
+    setLocalNotifications();
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <Tabs.Navigator
+          screenOptions={({ route }) => ({
+            tabBarIcon: ({ focused, color, size }) => {
+              let IconName;
+
+              if (route.name === "Decks") {
+                IconName = () => (
+                  <MaterialCommunityIcons
+                    name={"cards-variant"}
+                    size={size}
+                    color={color}
+                  />
+                );
+              } else if (route.name === "Add a Deck") {
+                IconName = () => (
+                  <AntDesign name={"addfile"} size={size} color={color} />
+                );
+              }
+
+              // You can return any component that you like here!
+              return <IconName />;
+            },
+          })}
+          tabBarOptions={{
+            activeTintColor: "#2196F3",
+            inactiveTintColor: "gray",
+          }}
+        >
+          <Tabs.Screen name="Decks" component={Decks} />
+          <Tabs.Screen name="Add a Deck" component={AddDeck} />
+        </Tabs.Navigator>
+      </NavigationContainer>
+    </Provider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
